99d64f3513f6df4f2db539e7ec18b12d96b48a97
ometman <revomet@gmail.com>
2023-11-15 15:05:46 +0000

    Create invoice, invoice_items and medical treament tables and their relationships

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,3 +16,26 @@ id  SERIAL PRIMARY KEY,
 type TIMESTAMP,
 name VARCHAR(50)
 );
+
+CREATE TABLE invoices(
+id  SERIAL PRIMARY KEY,
+total_amount DECIMAL(5, 2),
+generated_at TIMESTAMP,
+payed_at TIMESTAMP,
+medical_history_id INT UNIQUE REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoices_items(
+ id SERIAL PRIMARY KEY,
+ unit_price DECIMAL(5, 2),
+ quantity INT,
+ total_price DECIMAL(5, 2),
+ invoice_id INT REFERENCES invoices(id),
+ treatment_id INT REFERENCES treatments(id)
+);
+
+CREATE TABLE medical_treatment(
+    medical_history_id INT REFERENCES medical_histories(id),
+    medical_treatment_id INT REFERENCES treatments(id),
+    PRIMARY KEY(medical_history_id, medical_treatment_id)
+);
