8be200f7d94b78bc7d5dbfdf93a29d488bab6780
FilbertKimasa <filbertdan67@gmail.com>
2023-11-15 17:07:00 +0300

    Create table medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,10 @@ id  SERIAL PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 date_of_birth DATE
 );
+
+CREATE TABLE medical_histories(
+id  SERIAL PRIMARY KEY,
+admitted_at TIMESTAMP,
+patient_id INT REFERENCES patients(id),
+status VARCHAR(50)
+);
