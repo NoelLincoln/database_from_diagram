b497cdf7f4e2711db718913c933ea466376434ec
ometman <revomet@gmail.com>
2023-11-16 08:22:13 +0000

    Modify ids and create all 6 indexes on the 6 foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,32 +1,32 @@
 CREATE TABLE patients(
-id  SERIAL PRIMARY KEY,
+patient_id  SERIAL PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 date_of_birth DATE
 );
 
 CREATE TABLE medical_histories(
-id  SERIAL PRIMARY KEY,
+medical_histories_id  SERIAL PRIMARY KEY,
 admitted_at TIMESTAMP,
 patient_id INT REFERENCES patients(id),
 status VARCHAR(50)
 );
 
 CREATE TABLE treatments(
-id  SERIAL PRIMARY KEY,
+treament_id  SERIAL PRIMARY KEY,
 type VARCHAR(25),
 name VARCHAR(50)
 );
 
 CREATE TABLE invoices(
-id  SERIAL PRIMARY KEY,
+invoice_id  SERIAL PRIMARY KEY,
 total_amount DECIMAL(5, 2),
 generated_at TIMESTAMP,
 payed_at TIMESTAMP,
-medical_history_id INT UNIQUE REFERENCES medical_histories(id)
+medical_history_id INT UNIQUE REFERENCES medical_histories(medical_histories_id)
 );
 
 CREATE TABLE invoice_items(
- id SERIAL PRIMARY KEY,
+ invoice_items_id SERIAL PRIMARY KEY,
  unit_price DECIMAL(5, 2),
  quantity INT,
  total_price DECIMAL(5, 2),
@@ -35,11 +35,16 @@ CREATE TABLE invoice_items(
 );
 
 CREATE TABLE medical_treatment(
-    medical_history_id INT REFERENCES medical_histories(id),
-    medical_treatment_id INT REFERENCES treatments(id),
+    medical_history_id INT REFERENCES medical_histories(medical_histories_id),
+    medical_treatment_id INT REFERENCES treatments(treatment_id),
     FOREIGN KEY(medical_history_id, medical_treatment_id)
 );
 
 -- Create Index on foreign keys
-CREATE INDEX idx_med_history_id ON medical_histories(id);
-CREATE INDEX idx_med_treat_id ON treatments(id);
+
+CREATE INDEX idx_med_hist_patient_id ON medical_histories(patient_id);
+CREATE INDEX idx_invoices_med_hist_id ON invoices(medical_history_id);
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items(treatment_id);
+CREATE INDEX idx_med_treatment_id ON medical_treatment(medical_history_id);
+CREATE INDEX idx_med_treatment_id ON medical_treatment(medical_treatment_id);
